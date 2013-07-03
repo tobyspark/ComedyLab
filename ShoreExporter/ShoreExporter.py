903957cfcf3e8aa49898ea64245c1704d2d30b0a
Minos Katevas <minos.kat@gmail.com>
2013-07-03 10:09:52 +0100

    Add parseItem, parseLine and parseFile functions

--- ShoreExporter/ShoreExporter.py
+++ ShoreExporter/ShoreExporter.py
@@ -3,6 +3,48 @@
 import sys
 
 
+def parseFile(file):
+    '''Parse the file'''
+
+    # parse each line
+    for line in source:
+        parseLine(line)
+
+
+def parseLine(line):
+    '''Parse the line and...'''
+
+    # empty dict
+    dictionary = {}
+
+    # iterate through items
+    for item in line.split(" "):
+
+        # get the key and value from the item
+        key, value = parseItem(item)
+
+        # add to the dictionary
+        dictionary[key] = value
+
+    return dictionary
+
+
+def parseItem(item):
+    ''''''
+
+    itemList = item.split("=")
+
+    if len(itemList) == 3:
+        return itemList[0], itemList[2]
+
+    # handle the case where value is missing (e.g. 'Gender=')
+    elif len(itemList) == 2:
+        return itemList[0], None
+
+    else:
+        sys.exit('Error: structure of the input file is invalid. Item: ' + item)
+
+
 ''' main '''
 if __name__ == '__main__':
     
@@ -15,3 +57,9 @@ if __name__ == '__main__':
 
     # open the file
     source = open(filename, 'r')
+
+    # parse the file
+    parseFile(source)
+
+    # close the file
+    source.close()
