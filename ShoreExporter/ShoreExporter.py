77376f1b09c2720a13ceb6c6f63c736af215a5f9
Minos Katevas <minos.kat@gmail.com>
2013-07-03 09:25:05 +0100

    Add readfile from args functionality

--- ShoreExporter/ShoreExporter.py
+++ ShoreExporter/ShoreExporter.py
@@ -5,4 +5,13 @@ import sys
 
 ''' main '''
 if __name__ == '__main__':
-    pass
+    
+    # check if the the filename exists as a parameter
+    if (len(sys.argv) < 2):
+        sys.exit('Missing input file')
+
+    # read the filename from the 1st argument
+    filename = sys.argv[1]
+
+    # open the file
+    source = open(filename, 'r')
