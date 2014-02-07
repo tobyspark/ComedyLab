2c331b5c70d7e83953100d5f868627268db8d6d2
Christopher Frauenberger <c.frauenberger@sussex.ac.uk>
2014-02-07 11:33:54 +0100

    included markers for makeVideo to check the gaze

--- Vicon Exporter/makeVideo.m
+++ Vicon Exporter/makeVideo.m
@@ -1,4 +1,4 @@
-function makeVideo(dofs, data, samplerate, stopAt, speedUp, offsets)
+function makeVideo(dofs, data, samplerate, stopAt, speedUp, offsets, checkGazeOf)
 
 % Produces video from data
 % created 30. 1. 2014
@@ -15,6 +15,9 @@ function makeVideo(dofs, data, samplerate, stopAt, speedUp, offsets)
 % Output: video 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if nargin < 7
+    checkGazeOf = 1;
+end
 
 if nargin < 6
     offsets = cell(length(dofs)/12);
@@ -37,9 +40,15 @@ mov.FrameRate = samplerate/speedUp;
 open(mov);
 for i=1:speedUp:stopAt
     draw_data = reshape(data(i,:), 12, []);
+    % Colour mapping for all 3D scatter plots
+    colmap = repmat([0,0,0],length(draw_data(1,:)),1);
+    markersize = repmat (20,length(draw_data(1,:)),1);
+    colmap(checkGazeOf,:) = [0,1,0]; %colour the person we check the gaze for
+    markersize(checkGazeOf) = 40;
     ax = draw_data(1:3,:);
     ax = [ax; sqrt(sum(ax'.^2,2))'];
-    vec = [];
+    vecs = [];
+    lookAt = [];
     for j = 1:length(ax)
         if isempty(strfind(dofs{j*12}, 'Performer'))
             forward = [-1 0 0]; % Audience
@@ -47,11 +56,28 @@ for i=1:speedUp:stopAt
             forward = [1 0 0]; % Performer
         end
         rm = vrrotvec2mat(ax(:,j));
-        vec = [vec, (forward * rm * offsets{j})'];
+        vec = forward * rm * offsets{j};
+        vecs = [vecs, vec'];
+        pos = draw_data(4:6,j)';
+        angle = [];
+        for k = 1:length(ax)
+            if j~=k && isinfront(draw_data(4:6,k)', pos, vec)
+                % only check people within view (180 deg sphere)
+                dist = distancePointLine3d(draw_data(4:6,k)',[pos vec]);      
+                angle = [angle, asin(dist/norm(draw_data(4:6,k)'-pos))];
+            else
+                angle = [angle, inf];
+            end            
+        end
+        [minangle minindex] = min (angle);    
+        lookAt = [lookAt, minindex];
     end
+    % color the target of the person we check the gaze for
+    colmap(lookAt(checkGazeOf),:) = [1, 0, 0];
+    markersize(lookAt(checkGazeOf)) = 40;
     % only draws xyz ofthe root bone of participants
-    quiver3(draw_data(4,:), draw_data(5,:), draw_data(6,:), vec(1,:), vec(2,:), vec(3,:)); hold on;
-    scatter3(draw_data(4,:), draw_data(5,:), draw_data(6,:)); hold off;
+    quiver3(draw_data(4,:), draw_data(5,:), draw_data(6,:), vecs(1,:), vecs(2,:), vecs(3,:)); hold on;
+    scatter3(draw_data(4,:), draw_data(5,:), draw_data(6,:), markersize, colmap,'fill'); hold off;
 %     view (0,90);
     set (gca, 'XLim', [0 5000], 'YLim', [-2500 2500], 'ZLim', [0 2500])
     drawnow
